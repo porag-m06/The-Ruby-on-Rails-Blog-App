256dc911cdd73e0fd1f5b2e1e5b595e2c2795bcb
Md. Porag Sarkar <porag.m06@gmail.com>
2023-09-23 20:12:01 +0600

    user_spec test update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_numericality_of(:posts_counter).is_greater_than_or_equal_to(0) }
-  it { should have_many(:posts) }
-  it { should have_many(:likes) }
-  it { should have_many(:comments) }
+  let(:user) { User.create(name: 'Porag', posts_counter: 5) }
 
-  describe '#three_recent_posts' do
-    let(:user) { User.create(name: 'Test User', posts_counter: 0) }
-    let!(:older_post) { Post.create(title: 'Older Post', author: user, created_at: 1.week.ago) }
-    let!(:recent_posts) do
-      3.times.map do
-        Post.create(title: 'Recent Post', author: user, created_at: Time.zone.now)
-      end
-    end
-
-    it 'returns the three most recent posts' do
-      expect(user.three_recent_posts).to match_array(recent_posts)
-    end
+  it 'Should validate presence of name' do
+    user.name = nill
+    expect(user).to_not be_valid
   end
+
+  it 'To be valid posts_counter must always be an integer' do
+    user.posts_counter = 'p'
+    expect(user).to_not be_valid
+  end
+
+  it 'To be valid posts_counter should be greated than or equal to zero' do
+    user.posts_counter = -1
+    expect(user).to_not be_valid
+  end
+  
 end
