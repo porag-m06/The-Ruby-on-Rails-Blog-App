dab874f8a7a07db8de6ef42d5a55a3aed8f0ccbe
Md. Porag Sarkar <porag.m06@gmail.com>
2023-09-29 11:21:31 +0600

    post controller and views for all posts and post details added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
-    
-  def index  
+
+  # GET  /users/:user_id/posts
+  # Fetching the all posts of the user with id = :user_id
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
+  # GET  /users/:user_id/posts/:id
+  # Fetching the details post with post id = :id
   def show
+    @post = Post.find(params[:id])
   end
   
 end
