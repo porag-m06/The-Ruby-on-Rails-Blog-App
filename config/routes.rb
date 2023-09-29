d5bc615bb842bff646c4a228e8dabc4b832a818c
Md. Porag Sarkar <porag.m06@gmail.com>
2023-09-29 13:07:02 +0600

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  # get '/users', to: 'users#index', as: 'users'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+  resources :posts, only: [:index, :show]
 end
