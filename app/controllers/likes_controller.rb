7938104ff06b66fec628c7296a66e3f19d5d6416
Md. Porag Sarkar <porag.m06@gmail.com>
2023-10-07 22:42:37 +0600

    controller & form for  new comment and like added

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new
+    @post = Post.find(params[:id])
+    @like.post_id = @post.id
+    @like.user_id = current_user.id
+
+    if @like.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
   get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
   post '/users/:user_id/posts/create', to: 'posts#create', as: 'create_post'
-
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+  get '/users/:user_id/posts/:id/comments/new', to: 'comments#new', as: 'new_comment'
+  post '/users/:user_id/posts/:id/comments/create', to: 'comments#create', as: 'create_comments'
+
+  post '/users/:user_id/posts/:id/likes', to: 'likes#create', as: 'likes'
+  
 end
+
+
